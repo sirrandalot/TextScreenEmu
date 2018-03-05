3640702c3115b5437e7d86e5194abecc72f0b3d3
Marco Purich <purichmarco@yahoo.ca>
2018-03-05 17:38:07 -0500

    Created tile constructor

--- src/textScreenEmu/tilesets/Tile.java
+++ src/textScreenEmu/tilesets/Tile.java
@@ -2,4 +2,29 @@ package textScreenEmu.tilesets;
 
 public class Tile {
 
+	private int[] values;
+	
+	public final int tileWidth;
+	public final int tileHeight;
+	
+	public Tile(int width, int height){
+		
+		try{
+			
+			if(width <= 0 || width > 512)
+				throw new Exception("Width " + width + " out of range, must be between 1 and 512.");
+			
+			if(height <= 0 || height > 512)
+				throw new Exception("Height " + height + " out of range, must be between 1 and 512.");
+			
+		}catch(Exception e){
+			System.out.println("ERROR: " + e);
+			e.printStackTrace();
+		}
+		
+		tileWidth = width;
+		tileHeight = height;
+		
+		values = new int[tileWidth * tileHeight];
+	}
 }
