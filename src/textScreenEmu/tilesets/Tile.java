ab3bd38f0a64c1d6dbfba2b8390447718abe9b85
Marco Purich <purichmarco@yahoo.ca>
2018-03-05 19:15:07 -0500

    Finished Tile

--- src/textScreenEmu/tilesets/Tile.java
+++ src/textScreenEmu/tilesets/Tile.java
@@ -2,13 +2,33 @@ package textScreenEmu.tilesets;
 
 public class Tile {
 
-	private int[] values;
+	/**
+	 * The values of this tile.
+	 */
+	public int[] values;
 	
+	
+	/**
+	 * The width of this tile.
+	 */
 	public final int tileWidth;
+	
+	
+	/**
+	 * The height of this tile.
+	 */
 	public final int tileHeight;
 	
+	
+	/**
+	 * Constructor for the tile class using width, height and a value array.
+	 * @param width The width to give this tile.
+	 * @param height The height to give this tile.
+	 * @param vals The values to give this tile.
+	 */
 	public Tile(int width, int height, int[] vals){
 		
+		//Check if the input values are appropriate
 		try{
 			
 			if(width <= 0 || width > 512)
@@ -31,6 +51,11 @@ public class Tile {
 		values = vals.clone();
 	}
 	
+	/**
+	 * Constructor for Tile class using just width and height, calls the other constructor with a blank array for values.
+	 * @param width The width to give this tile.
+	 * @param height The height to give this tile.
+	 */
 	public Tile(int width, int height){
 		this(width, height, new int[width*height]);
 	}
