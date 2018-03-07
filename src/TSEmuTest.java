f3cd9dfec25c40b19d59720bcce9daf2500be476
Marco Purich <purichmarco@yahoo.ca>
2018-03-07 10:59:14 -0500

    Added hardcoded tilesets

--- src/TSEmuTest.java
+++ src/TSEmuTest.java
@@ -1,6 +1,9 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -19,7 +22,7 @@ public class TSEmuTest {
 		
 		JFrame mainFrame = new JFrame();
 		
-		Screen s = new Screen(32, 8, 4, TilesetFactory.createTileset(TilesetPredef.CODEPAGE473_9x16), PaletteFactory.createPalette(PalettePredef.VGA_16));
+		Screen s = new Screen(32, 16, 2, TilesetFactory.createTileset(TilesetPredef.CODEPAGE473_8x8), PaletteFactory.createPalette(PalettePredef.VGA_16));
 		
 		mainFrame.add(s);
 		
@@ -47,11 +50,11 @@ public class TSEmuTest {
 		
 		s.repaint();
 		
-		getDataFromImage("cp437_9x16", 9, 16);
-		getDataFromImage("cp437_8x8", 8, 8);
-		getDataFromImage("cp437_10x10", 10, 10);
-		getDataFromImage("cp437_12x12", 12, 12);
-		getDataFromImage("cp437_16x16", 16, 16);
+		//getDataFromImage("cp437_9x16", 9, 16);
+		//getDataFromImage("cp437_8x8", 8, 8);
+		//getDataFromImage("cp437_10x10", 10, 10);
+		//getDataFromImage("cp437_12x12", 12, 12);
+		//getDataFromImage("cp437_16x16", 16, 16);
 		
 	}
 	
@@ -64,7 +67,42 @@ public class TSEmuTest {
 			e.printStackTrace();
 		}
 		
-		
+		try {
+			PrintWriter writer = new PrintWriter("C:/Users/puric/Desktop/PictureStuff/Tilesets/" + name + ".txt", "UTF-8");
+			
+			for(int ty = 0; ty < 16; ty++){
+				for(int tx = 0; tx < 16; tx++){
+					writer.print("new Tile(" + x + ", " + y + ", " + "new int[]{\n");
+					
+					for(int j = 0; j < y; j++){
+						for(int i = 0; i < x; i++){
+							
+							int r = buf.getRGB(x*tx + i, y*ty + j) & 0xFF;
+							
+							if(r > 0)
+								writer.print(1);
+							else
+								writer.print(0);
+							
+							if(i != x-1 || j != y-1)
+								writer.print(", ");
+						}
+						writer.print("\n");
+					}
+					
+					
+					writer.print("})");
+					
+					if(ty != 15 || tx != 15)
+						writer.print(",\n");
+						
+				}
+			}
+			
+			writer.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		
 		
