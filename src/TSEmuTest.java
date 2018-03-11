97b44e3962925ea1f9d07f40e9676ced6a5ffc18
Marco Purich <purichmarco@yahoo.ca>
2018-03-11 19:47:23 -0400

    created proper images for all palettes

--- src/TSEmuTest.java
+++ src/TSEmuTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
+import textScreenEmu.Palette;
 import textScreenEmu.PaletteFactory;
 import textScreenEmu.PalettePredef;
 import textScreenEmu.Screen;
@@ -21,7 +22,10 @@ public class TSEmuTest {
 		TilesetPredef tsPredef = TilesetPredef.CODEPAGE437_16x16;
 		PalettePredef plPredef = PalettePredef.PHOSPHOR_8;
 		
-		s = new Screen(16, 16, 2, TilesetFactory.createTileset(tsPredef), PaletteFactory.createPalette(plPredef));
+		Palette pal = PaletteFactory.createPalette(plPredef);
+		
+		//s = new Screen(16, 16, 2, TilesetFactory.createTileset(tsPredef), PaletteFactory.createPalette(plPredef));
+		s = new Screen(8, 1, 1, TilesetFactory.createTileset(1, 1, 2, new int[]{0, 1}), pal);
 		
 		mainFrame.add(s);
 		
@@ -44,18 +48,18 @@ public class TSEmuTest {
 		
 		//s.drawTileset(false);
 		
-		//s.drawPalette(false);
+		s.drawPalette(false);
 		
-		for(int i = 0; i < 10; i++){
-			s.drawRandomTile(i, i, i*((long)(Math.random()*5000)));
-		}
+//		for(int i = 0; i < 10; i++){
+//			s.drawRandomTile(i, i, i*((long)(Math.random()*5000)));
+//		}
+//		
+//		s.drawTile(10,  2, '+');
 		
-		s.drawTile(10,  2, '+');
 		
+		//s.setForegroundColour(3);
 		
-		s.setForegroundColour(3);
-		
-		s.drawString(2,  5,  "Testing sTrIng!!?? Testing out this new draw string feature man this is pretty cool but I have to keep writing in order to test it properly.", true, true);
+		//s.drawString(2,  5,  "Testing sTrIng!!?? Testing out this new draw string feature man this is pretty cool but I have to keep writing in order to test it properly.", true, true);
 		
 		
 		s.repaint();
@@ -66,7 +70,7 @@ public class TSEmuTest {
 //			e1.printStackTrace();
 //		}
 //		
-//		File outputfile = new File("Palette_" + plPredef.toString() + ".png");
+//		File outputfile = new File("pal_" + plPredef.toString() + ".png");
 //        try {
 //			ImageIO.write(s.BIScaled, "png", outputfile);
 //		} catch (IOException e) {
